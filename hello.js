e288d4b0a0790f5c8dc4c2a69fb7607d29f419ef
Ibrahim <ibrahimabusada59@gmail.com>
2025-01-02 18:38:23 +0200

    add the stashed code

--- hello.js
+++ hello.js
@@ -1,9 +1,14 @@
 console.log('Hello, Git!');
 console.log('Hello, GitHub!');
 
+
 // implement the bug fix 
 // implement the bug fix 
 // implement the bug fix 
 // implement the bug fix 
 
-
+// stashing my work
+// until i fix the bug
+// then calling it back 
+// to work on it and finish
+// then merging
